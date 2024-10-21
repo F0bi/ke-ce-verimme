c926bf701a3462f96a946878246dbe679481a7e6
Fobi <29637951+F0bi@users.noreply.github.com>
2024-10-21 21:15:01 +0200

    Update cb01 url

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sys import platform
 summaryPageName = 'index.html'
 urlsToScrape = {
     'staseraInTvURL': 'https://www.staseraintv.com',
-    'cb01URL': 'https://cb01.church'
+    'cb01URL': 'https://cb01.nexus'
 }
 numberOfPagesToAnalyze = 3 # first 3 pages
 env = ''
